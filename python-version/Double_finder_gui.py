002652430370ae2aa1617015c1df150658071e08
NHLOCAL <nh.local11@gmail.com>
2023-10-05 05:33:17 +0300

    Update Double_finder_gui.py

--- python-version/Double_finder_gui.py
+++ python-version/Double_finder_gui.py
@@ -1,9 +1,12 @@
 import os
 import sys
 import tkinter as tk
-from tkinter import messagebox, Listbox, Scrollbar, Button
+from tkinter import messagebox, Listbox, Scrollbar, Button, Menu
+from tkinter import ttk
+from collections import defaultdict
 
 # Constants
+START_OFFSETS = [(145, 1985), (2778, 3800)]
 SUPPORTED_EXTENSIONS = (".mp3", ".wma", ".wav")
 
 # Global variables
@@ -38,7 +41,21 @@ def main():
     
     # Create a Tkinter window
     window = tk.Tk()
-    window.title("Duplicate Songs Deleter")
+    window.title("מוצא הכפולים")  # Title in Hebrew
+    
+    # Set RTL layout
+    window.tk_setPalette(background="#EAF2FF")  # Light blue background
+    
+    # Create a menu in Hebrew
+    menu = Menu(window)
+    window.config(menu=menu)
+    file_menu = Menu(menu)
+    menu.add_cascade(label="קובץ", menu=file_menu)
+    file_menu.add_command(label="יציאה", command=window.quit)
+    
+    # Create a title label
+    title_label = ttk.Label(window, text="מוצא הכפולים", font=("Arial", 20, "bold"))
+    title_label.pack(pady=10)
     
     # Create a listbox and scrollbar for displaying duplicate songs
     listbox = Listbox(window, selectmode=tk.MULTIPLE)
@@ -50,23 +67,31 @@ def main():
     listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
     scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
     
-    delete_button = Button(window, text="Delete Selected", command=delete_selected_files)
-    delete_button.pack()
-    
     files_list = []
+    file_dict = defaultdict(list)
     
     for root, dirs, files in os.walk(dir_path):
         for file in files:
-            len_item = len(files_list) + 1
-            show_len = len_item * 100 // len_dirs
-            print(f"{show_len}% הושלמו", end='\r')
-            
             if not file.endswith(SUPPORTED_EXTENSIONS):
                 continue
             
             id_file = read_binary_slice(os.path.join(root, file))
             files_list.append((os.path.join(root, file), id_file))
-            listbox.insert(tk.END, os.path.join(root, file))
+            file_dict[id_file].append(os.path.join(root, file))
+    
+    duplicate_files_found = False
+    
+    for id_item, file_paths in file_dict.items():
+        if len(file_paths) > 1:
+            duplicate_files_found = True
+            for file_path in file_paths:
+                listbox.insert(tk.END, file_path)
+    
+    if not duplicate_files_found:
+        listbox.insert(tk.END, "אין קבצים כפולים")
+    else:
+        delete_button = Button(window, text="מחק נבחרים", command=delete_selected_files)
+        delete_button.pack()
     
     window.mainloop()
     
