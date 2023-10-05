89d92b7074291a2bc0eb975c1a18637ae2f0c7db
NHLOCAL <nh.local11@gmail.com>
2023-10-05 05:02:20 +0300

    ייעול וכתיבה מחדש של הקוד

--- python-version/Find_duplic_ample.py
+++ python-version/Find_duplic_ample.py
@@ -1,71 +1,54 @@
-import os, sys
+import os
+import sys
 
-def duplic_scan(myfile):
-# פונקציה זו עוברת על הקוד הבינארי של הקובץ ושומרת מדגם שלו למשתנה
-    break_num = 0
-    id_file = "id:"
-    with open(myfile, 'rb') as input_file:
-        for line in input_file:
-            break_num += 1
-            if ((break_num > 145) and (break_num < 1985)) or ((break_num > 2778) and (break_num < 4900)):
-                continue
-            line_to_str = str(line)
-            id_file += line_to_str[7:83:19]
-            if 5530 == break_num:
-                break
-            
-    return id_file
+# Constants
+START_OFFSETS = [(145, 1985), (2778, 3800)]
+SUPPORTED_EXTENSIONS = (".mp3", ".wma", ".wav")
 
+def read_binary_slice(file_path):
+    with open(file_path, 'rb') as file:
+        binary_data = file.read()
+        return binary_data
 
 def main():
-# פונקציה זו עוברת על קבוצת קבצים שבתיקיה ומפעילה עליהם את הפונקציה duplic_scan
     dir_path = str(sys.argv[1])
     if (dir_path != "") and (os.path.exists(dir_path)):
         print("\n>>" + sys.argv[1])
 
-    len_item = 0
     len_dirs = sum(len(files) for _, _, files in os.walk(dir_path))
     files_list = []
-    id_list = []
-    dict_to_del = {}
-    file_num = 0
     
-    # מעבר על רשימת הקבצים והוספת דגימה של הקידוד שלהם למשתנה
     for root, dirs, files in os.walk(dir_path):
         for file in files:
-            # תצוגת אחוזים מתחלפת
-            len_item += 1
+            len_item = len(files_list) + 1
             show_len = len_item * 100 // len_dirs
-            print(str(show_len) + "% " + "הושלמו",end='\r')
-            # בדיקה אם שם הקובץ הפנימי מכיל סיומות ספציפיות
-            if not file.endswith((".mp3",".wma", ".wav")):
-                continue
-            # הפעלת פונקציה למיון קבצים דומים
-            id_file = duplic_scan(os.path.join(root, file))
+            print(f"{show_len}% הושלמו", end='\r')
             
-            # הוספת התוצאות לרשימה
+            if not file.endswith(SUPPORTED_EXTENSIONS):
+                continue
+            
+            id_file = read_binary_slice(os.path.join(root, file))
             files_list.append((os.path.join(root, file), id_file))
     
-    # יצירת רשימת ה-ID של הקבצים    
-    for file_item, id_item in files_list: id_list.append(id_item)
+    id_list = [id_item for _, id_item in files_list]
+    dict_to_del = {}
     
-    # בדיקה אם ה-ID קיים יותר מפעם אחת
     for file_item, id_item in files_list:
         if id_list.count(id_item) >= 2:
-            file_num += 1
-            dict_to_del.update({file_num: file_item})
+            file_num = len(dict_to_del) + 1
+            dict_to_del[file_num] = file_item
             print_file_item = file_item.replace(dir_path + "\\", "")
-            print(str(file_num) + ": " + print_file_item)
-            
-    if file_num >= 1:
+            print(f"{file_num}: {print_file_item}")
+    
+    if dict_to_del:
         select_file = input("\nהכנס מספר רצוי בכדי למחוק קובץ\nניתן להכניס כמה ספרות בהפרדה של רווח ביניהם" + "\n>>>")
         select_file = select_file.split()
         try:
             for del_item in select_file:
                 os.remove(dict_to_del[int(del_item)])
-                print(dict_to_del[int(del_item)] + "  -- נמחק!")
+                print(f"{dict_to_del[int(del_item)]}  -- נמחק!")
         except Exception as e:
-            print("עליך להכניס מספר בכדי למחוק קובץ מסוים!")
+            print("עליך להכניס מספר בכדי למחוק קובץ מסויים!")
     else:
         print("לא נמצא דבר!")
 
--- python-version/python_run_tree.bat
+++ python-version/python_run_tree.bat
@@ -9,11 +9,11 @@ if %errorlevel% == 2 goto :b
 if %errorlevel% == 1 goto :a
 
 :a
-for /d %%i in ("J:\שמע\כל המוזיקה\*") do "%~dp0\Find_duplic_ample.py" "%%~i"
+for /d %%i in ("D:\שמע\כל המוזיקה\*") do "%~dp0\Find_duplic_ample.py" "%%~i"
 pause
 exit
 
 :b
-for /d %%i in ("J:\שמע\כל המוזיקה\*") do "%~dp0\Find_duplic_names.py" "%%~i"
+for /d %%i in ("D:\שמע\כל המוזיקה\*") do "%~dp0\Find_duplic_names.py" "%%~i"
 pause
 exit
